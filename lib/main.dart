bc1e3147694624528be2dd97534cbeae219eb175
Kriti Banka <bankakriti10@gmail.com>
2023-03-05 12:42:56 +0530

    1st commit

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:bookwarts/screens/sign%20up.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+void main(){
+  runApp(MaterialApp(routes:
+  {
+    "/": (context) => SignUp(),
+  },
+  ));
+}
